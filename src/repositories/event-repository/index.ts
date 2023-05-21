3d9ad2c1526a7174f712bf36cfa4d434ede58730
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2023-05-21 11:02:45 -0300

    feat: event

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -3,9 +3,11 @@ import { client } from '@/config/redis';
 
 async function findFirst() {
   const eventRedis = await client.get('event');
+
   if (eventRedis) {
     return JSON.parse(eventRedis);
   }
+
   const value = await prisma.event.findFirst();
   await client.set('event', JSON.stringify(value));
   return value;
