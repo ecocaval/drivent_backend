206c497a72879ee2e4076cb05c69f30be3f7f7df
nikarcosta <monique_r_costa@hotmail.com>
2023-05-29 11:47:07 -0300

    feat: tests for the get/activities/date endpoint

--- tests/integration/activities.test.ts
+++ tests/integration/activities.test.ts
@@ -1,19 +1,7 @@
 import httpStatus from 'http-status';
 import faker from '@faker-js/faker';
 import supertest from 'supertest';
-import * as jwt from 'jsonwebtoken';
-import { TicketStatus } from '.prisma/client';
-import { cleanDb, generateValidToken } from '../helpers';
-import {
-  createEnrollmentWithAddress,
-  createHotel,
-  createPayment,
-  createRoomWithHotelId,
-  createTicket,
-  createTicketTypeRemote,
-  createTicketTypeWithHotel,
-  createUser,
-} from '../factories';
+import { cleanDb } from '../helpers';
 import app, { init } from '@/app';
 
 beforeAll(async () => {
@@ -25,25 +13,6 @@ beforeEach(async () => {
 });
 
 const server = supertest(app);
-/*
-describe('GET /activities', () => {
-    it('should respond with status 200 and a list of activities', async () => {
-      const response = await server.get('/activities');
-
-      expect(response.status).toEqual(httpStatus.OK);
-
-      expect(response.body).toEqual([
-        {
-          id: createdHotel.id,
-          name: createdHotel.name,
-          image: createdHotel.image,
-          createdAt: createdHotel.createdAt.toISOString(),
-          updatedAt: createdHotel.updatedAt.toISOString(),
-        },
-      ]);
-    });
-});
-*/
 
 import activitiesRepository from '@/repositories/activity-repository';
 
@@ -95,6 +64,31 @@ describe('GET /activities', () => {
     jest.spyOn(activitiesRepository, 'getActivities').mockRejectedValue(new Error('Database connection error'));
 
     const response = await server.get('/activities');
+
+    expect(response.status).toBe(500);
+  });
+});
+
+describe('GET /activities/dates', () => {
+  it('should respond with status 200 and a list of activity dates', async () => {
+    jest.spyOn(activitiesRepository, 'getActivitiesDate').mockResolvedValue([
+      { id: 1, weekDay: 'MONDAY', month: 'MAY', monthDay: 31 },
+      { id: 2, weekDay: 'TUESDAY', month: 'JUNE', monthDay: 1 },
+    ]);
+
+    const response = await server.get('/activities/dates');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([
+      { id: 1, weekDay: 'MONDAY', month: 5, monthDay: 31 },
+      { id: 2, weekDay: 'TUESDAY', month: 6, monthDay: 1 },
+    ]);
+  });
+
+  it('should handle errors and pass them to the error handling middleware', async () => {
+    jest.spyOn(activitiesRepository, 'getActivitiesDate').mockRejectedValue(new Error('Database connection error'));
+
+    const response = await server.get('/activities/dates');
 
     expect(response.status).toBe(500);
   });
